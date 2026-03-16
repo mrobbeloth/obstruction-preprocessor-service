1b2a3a479b01f0126d71631a2d9b8a7da3e5d3ae
mrobbeloth <michael@robbeloth.com>
2026-03-15 21:21:55 -0400

    Notes for older desktops running nvidia gtx 970

--- design/build_opencv.sh
+++ design/build_opencv.sh
@@ -4,6 +4,7 @@
 OPENCV_VERSION="4.11.0"
 COMPUTE_CAP="5.2" # Change this for different GPUs
 
+cmake --build ../ --target clean
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D CMAKE_INSTALL_PREFIX=/usr/local \
       -D WITH_CUDA=ON \
@@ -11,4 +12,5 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D OPENCV_DNN_CUDA=ON \
       -D CUDA_ARCH_BIN=$COMPUTE_CAP \
       -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+      -D CUDA_NVCC_FLAGS="-ccbin /usr/bin/gcc-11" \
       ..
--- design/debian_cuda_config.cmake
+++ design/debian_cuda_config.cmake
@@ -2,4 +2,5 @@ set(WITH_CUDA ON CACHE BOOL "")
 set(WITH_CUDNN ON CACHE BOOL "")
 set(OPENCV_DNN_CUDA ON CACHE BOOL "")
 set(CUDA_ARCH_BIN "5.2 6.1 7.5 8.6" CACHE STRING "")
+# on GTX 970 systems, use set(CUDA_NVCC_FLAGS "-ccbin /usr/bin/gcc-11")
 # Add other flags here using the 'set(... CACHE ...)' syntax
